71a9cd887741881782f788e1a34d1ca0cee2a2f3
Ruby Chen <ruby@yun-ru.com>
2016-08-25 17:41:13 +0800

    add remove method

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
 const ADD_TODO = "ADD_TODO";
 const REMOVE_TODO = "REMOVE_TODO";
+const FINISH_TODO = "FINISH_TODO";
 
 class Action {
     addTodo(todo) {
         return {type: ADD_TODO, todo}
     }
-    removeTodo(id){
+    finishTodo(id) {
+        return {type: FINISH_TODO, id}
+    }
+    removeTodo(id) {
         return {type: REMOVE_TODO, id}
     }
 }
@@ -30,7 +34,7 @@ class Store {
                     Object.assign({},this.state,{todos:
                         this.state.todos.concat([{id: Date.now(), title: action.todo, finished: false}])
                     });
-            case REMOVE_TODO:
+            case FINISH_TODO:
                 return this.state =
                     Object.assign({},this.state,{todos:
                         this.state.todos.map(todo=>{
@@ -41,6 +45,11 @@ class Store {
                                 return todo;
                             }
                         })
+                    });
+            case REMOVE_TODO:
+                return this.state =
+                    Object.assign({},this.state,{todos:
+                        this.state.todos.filter(todo=>todo.id!==action.id)
                     });
 
             default:
@@ -64,16 +73,24 @@ class Main {
         this.$myInput = $("#myInput");
         this.$myBtn = $("#myBtn");
         this.template = $("#template").html();
+        
     }
     eventBinding() {
         $(window).on("load",this.render.bind(this));
         this.$myBtn.on("click",this.handleSubmit.bind(this));
         this.$myInput.on("keyup",e=>{e.keyCode===13 && this.handleSubmit()});
-        this.$todoList.on("click",".delete",e=>this.removeTodo(e))
+        this.$todoList.on("click","button.finish",e=>this.finishTodo(e));
+        this.$todoList.on("click","button.delete",e=>this.removeTodo(e));
+
     }
     removeTodo(e) {
         const id = $(e.target).data("id");
         store.dispatch(action.removeTodo(id));
+        this.render();
+    }
+    finishTodo(e) {
+        const id = $(e.target).data("id");
+        store.dispatch(action.finishTodo(id));
         this.render();
     }
     handleSubmit() {
